773dd66425d345f734176295b395368c3effa831
Artem <pushakartem@gmail.com>
2021-01-22 12:44:35 +0200

    i add comment with callback function

--- source/js/script.js
+++ source/js/script.js
@@ -150,6 +150,34 @@ writeYourGenres();
 
 
 
+// CALLBACK FUNCTION //
+
+/* 
+function learnJS(lang, callback) {
+    console.log(`Я вчу ${lang}!`);
+    callback();
+}
+
+function done() {
+    console.log('done');
+}
+
+learnJS('JavaScript', done); // now i type function 'done' without "()" ( like: "done();" ), because this function will be come to "callback(here)" in first function "learnJS" //
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //OR//
