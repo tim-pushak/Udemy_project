08a1964bf7832fc1784b22f8bb712811a5bd4326
Artem <pushakartem@gmail.com>
2021-01-29 17:44:00 +0200

    added some objects and write down MY PERSONAL OBJECT LIKE ^^ in the comments

--- source/js/script.js
+++ source/js/script.js
@@ -144,8 +144,92 @@ writeYourGenres();
 
 
 
+// OBJECTS //
+
+/* const options = {
+    name: 'test', // "name: 'test'" - is method, "name" - is key or property, 'test' - is value
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('test');
+    }
+}
+
+options.makeTest(); 
+
+const {border, bg} = options.colors; // destructuring code, the first thing, we type what do we want to get then type from what object we will do this
+
+console.log(border); */
+
+// console.log(Object.keys(options).length); //important thing and most useble in js
 
 
+/* console.log(options.name);
+
+delete options.name;
+
+console.log(options); */
+
+
+
+/* console.log(options["colors"]["border"]); */ // it works but don`t practic and cool in usability
+
+
+/*
+let counter = 0;
+
+for (let key in options) {
+    if(typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Property ${i} have value ${options[key][i]}`);
+            
+        }
+    } else {
+        console.log(`Property ${key} have value ${options[key]}`);
+        counter++;
+    }
+};
+
+console.log(counter); */
+
+
+
+// MY PERSONAL OBJECT LIKE ^^ //
+
+/* 
+const features = {
+    name: 'hello',
+    age: 23,
+    width: 1123,
+    height: 1123,
+    colors: {
+        border: 'red',
+        bg: 'blue'
+    },
+    makePlan: function() {
+        let plan = 25;
+
+        if(plan === 15) {
+            let cool = 'oh men, it`s 15';
+            console.log(cool);
+        } else if(plan === 25) {
+            let wow = 'not bad';
+            console.log(wow);
+        } else {
+            let what = 'error bro';
+            console.log(what);
+        }
+    }
+}
+
+features.makePlan();
+
+
+console.log(features); */
 
 
 
@@ -164,9 +248,6 @@ function done() {
 
 learnJS('JavaScript', done); // now i type function 'done' without "()" ( like: "done();" ), because this function will be come to "callback(here)" in first function "learnJS" //
  */
-
-
-
 
 
 
