581470bf7cd76ac97df9e984144be4587e3a8f94
Artem <pushakartem@gmail.com>
2021-02-06 16:59:35 +0200

    added new js file array_prototype.js and move here code with diffrent methods of making prototype on arrayes

--- source/js/study.js
+++ source/js/study.js
@@ -1,5 +1,63 @@
 "use strict";
 
+// Основы ООП, прототипно-ориентированное наследование //
+// Array prototype //
+
+/* const soldiers = {
+    health: 400,
+    armor: 100,
+    sayHi: function() {
+        console.log("Hello");
+    }
+}; */
+
+// 3 methods make prototype //
+// 1 the best //
+/* const john = Object.create(soldiers); */
+//
+
+// 2 //
+// Object.setPrototypeOf(john, soldiers); // use this //
+//
+
+// 3 bad //
+    /* john.__proto__ = soldiers; */ // it's very old method, not usually use ^^ here alternative ^^//
+//
+
+/* const john = {
+    health: 200
+}; */ // in programming we don't create things like that, we made things like these ^^ //
+
+// john.sayHi();
+
+// console.log(john);
+// console.log(john.armor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // SPREAD operator ES6 - ES9 //////////  COPY  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
