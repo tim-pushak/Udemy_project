68d3f0f300a82be442b14efa4790bc647403c19a
Artem <pushakartem@gmail.com>
2021-02-12 17:40:57 +0200

    added new js file <getElements28.js> and write new comment <(28)Getelements from the page>. also i still don't know why i have a bag <expression expected>

--- source/js/study.js
+++ source/js/study.js
@@ -1,6 +1,54 @@
 "use strict";
+/* 
+// (28) Get elements from the page
 
-// Math examples
+// by ID
+const box = document.getElementById('box');
+console.log(box);
+
+// by TagName
+const btns = document.getElementsByTagName('button'); // now we get not just one element, but arrive or "HTML Collection()", with different elements, that page had with tag "button" 
+const btns2 = document.getElementsByTagName('button');    
+
+console.log(btns);
+console.log(btns2[2]); // if i wanna just only second button and this btn have index "1"
+
+// by className
+const vasya = document.getElementsByClassName('vasya');
+
+console.log(vasya);
+
+// also exist "getElementByName" but it not usability
+
+
+
+
+
+
+// by selectorName (className) with forEach
+// тут виводиться всі елементи окремо
+const hearts = document.querySelectorAll('.heart'); // note that i type "." before class name and don't forget about ''
+
+hearts.forEach(item => { // callback function where "item" is "hearts" 
+    console.log(item); 
+}); // i can also just write console.log(hearts); but method "forEach" can allow me to return each elements singly
+
+// а тут виводиться тільки перший елемент зі списку
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+ */
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// (27) Math examples 
 
 /* 1)
 let x = 5;
