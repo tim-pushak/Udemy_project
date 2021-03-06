89a1c0d97d4c606d29ed62cd561b9468270d9dd8
Artem <pushakartem@gmail.com>
2021-03-06 20:38:20 +0200

    added new comment and js file <dynamic&classList_37.js>, nice features of classList and event.target, addEventListener

--- source/js/study.js
+++ source/js/study.js
@@ -2,6 +2,57 @@
 
 
 
+// (37) dynamic classList and events
+
+// classList and item()
+// add(), remove() 
+// toggle() - він додає елемент якщо його немає або видаляє якщо він є
+// contains
+
+const btns = document.querySelectorAll('button'),
+      wrappers = document.querySelector('.obolonka');
+
+// console.log(btns[0].classList.item(0));
+// console.log(btns[0].classList.add('red'));
+// console.log(btns[0].classList.remove('vasya'));
+// console.log(btns[0].classList.toggle('ivan'));
+
+// if (btns[0].classList.contains('red')) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+
+btns[0].addEventListener('click', () => {
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red');
+    } else {
+        btns[1].classList.remove('red');
+    }
+
+    // or but not understandble
+    // btns[1].classList.toggle('red');
+}); 
+
+// className is not useble !!!
+
+wrappers.addEventListener('click', (event) => {
+    // console.log(event.target);
+    if (event.target && event.target.tagName == 'BUTTON') {
+        console.log('Hello');
+    }
+});
+
+
+const btn = document.createElement('button');
+btn.classList.add('blue');
+wrappers.append(btn);
+btn.textContent = '7';
+
+
+
+
 
 // (35) Async & Defer in Scripts
 
