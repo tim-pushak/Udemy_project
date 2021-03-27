1db70776ead31ecdb634ee906ec82c7487e9e029
Artem <pushakartem@gmail.com>
2021-03-27 15:20:14 +0200

    added new the same changes to 51_file.js <about JSON>

--- source/js/study.js
+++ source/js/study.js
@@ -5,23 +5,23 @@
 
 // (51) json format a push of the data
 
-const persone = {
+/* const persone = {
     name: 'Alex',
     tel: '8 800 355 35 35',
     parents: {
         mum: 'Olga',
         dad: 'Mike'
     }
-};
+}; */
 
 // console.log(JSON.stringify(persone));
 // console.log(JSON.parse(JSON.stringify(persone)));
 
-const clone = JSON.parse(JSON.stringify(persone));
+/* const clone = JSON.parse(JSON.stringify(persone));
 clone.parents.mum = 'Ann';
 
 console.log(persone);
-console.log(clone);
+console.log(clone); */
 
 
 
--- source/js/study/51_JSON_push_of_the_data.js
+++ source/js/study/51_JSON_push_of_the_data.js
@@ -7,8 +7,18 @@
 
 const persone = {
     name: 'Alex',
-    tel: '8 800 355 35 35'
+    tel: '8 800 355 35 35',
+    parents: {
+        mum: 'Olga',
+        dad: 'Mike'
+    }
 };
 
-console.log(JSON.stringify(persone));
-console.log(JSON.parse(JSON.stringify(persone)));
+// console.log(JSON.stringify(persone));
+// console.log(JSON.parse(JSON.stringify(persone)));
+
+const clone = JSON.parse(JSON.stringify(persone));
+clone.parents.mum = 'Ann';
+
+console.log(persone);
+console.log(clone);
