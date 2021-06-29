49094636335801f84e7099e7050527503ea28963
Artem <pushakartem@gmail.com>
2021-06-29 16:16:33 +0300

    added some changes to study.js and created new html file <calc.html>

--- source/js/study.js
+++ source/js/study.js
@@ -3,6 +3,51 @@
 
 
 
+// (52) Ajax & server
+
+// XMLHttpRequest()  
+// 'readystatechange', () => {};
+
+
+
+const inputRub = docuemnt.querySelector('#rub'),
+      inputUsd = docuemnt.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    // request.open(method, url, async, loigin, pass);
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response); 
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Smth went wrong";
+        }
+    });
+
+    // status
+    // statusText
+    // response
+    // readyState
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 // (51) json format a push of the data
 
 /* const persone = {
