72561ba0013d2bf44dd44cb7bc088a2eb9d66f2f
Artem <pushakartem@gmail.com>
2021-07-05 17:59:41 +0300

    added new function about exchange, lesson 52

--- source/js/study.js
+++ source/js/study.js
@@ -8,7 +8,6 @@
 // XMLHttpRequest()  
 // 'readystatechange', () => {};
 
-
 const inputRub = document.querySelector('#rub'),
       inputUsd = document.querySelector('#usd');
 
@@ -16,28 +15,28 @@ inputRub.addEventListener('input', () => {
     const request = new XMLHttpRequest();
 
 
-    /* request.open(method, url, async, login, password); */
-    request.open(GET, js/current.json);
-    // request.open(method, url, async, loigin, pass);
-    request.open('GET', 'js/current1.json');
+    /* request.open('method', 'url', 'async', 'login', 'password'); */
+    request.open('GET', 'js/current.json');
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
 
     request.addEventListener('load', () => {
         if (request.status === 200) {
-            const data = JSON.parse(request.response); 
+            console.log(request.response);
+            const data = JSON.parse(request.response);
             inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
         } else {
             inputUsd.value = "Smth went wrong";
         }
-    });
+    
+    }); // відслідковує поточний стан запиту, (4 - readyState - done), (200 - status)
 
-    // status
-    // statusText
-    // response
-    // readyState
+
+    //status (error 404 and so on check it in wikipedia)
+    //statusText (ok, not found (404 ERROR))
+    //response (відповідь, відповідь від сервера)
+    //readyState (поточний стан запиту, позначається цифрою)
 });
-
 
 
 
